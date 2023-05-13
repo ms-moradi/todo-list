a4357ec8712733e5d2885e80df782edb5e86c4f1
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:41:47 +0430

    create and add imports

---
+++ src/page/home/home.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from "react";
+import Header from "../../component/Header/Header";
+import Main from "../../component/Main/Main";
+import Grid from "@mui/material/Grid";
+
+
+const Home =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default Home;
