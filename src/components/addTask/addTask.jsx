fa8e5e9d8743c1386299fb9d3231a2de44f98e7e
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:18:31 +0430

    add props

--- src/components/addTask/addTask.jsx
+++ src/components/addTask/addTask.jsx
@@ -6,10 +6,10 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-const AddTask =()=>{
+const AddTask =({form ,setForm , mode, setMode,setOpen})=>{
     return(
         <>
-        
+
         </>
     )
 }
