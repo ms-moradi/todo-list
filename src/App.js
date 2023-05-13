dace953d19ce31964dc57b198f3fbaef78c1ae8c
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:58:10 +0430

    add Provider and store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Home from '../src/page/home/home';
+import {Provider} from 'react-redux';
+import {store} from "./component/redux/store";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Provider store={store}>
+      <Home/>
+    </Provider>
   );
 }
 
