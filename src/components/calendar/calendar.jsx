b05ffe0254c51fea9eb470400d25fa57c164b596
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:36:00 +0430

    add props

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -3,7 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './calendar.css';
 
-const Calendar =()=>{
+const Calendar =({date, setDate})=>{
     return(
         <>
         </>
