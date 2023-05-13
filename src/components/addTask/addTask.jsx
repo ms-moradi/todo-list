951a9463832e64d7f3d7caf3597242622438d8e5
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:19:32 +0430

    add elements and styles

--- src/components/addTask/addTask.jsx
+++ src/components/addTask/addTask.jsx
@@ -8,9 +8,50 @@ import Typography from "@mui/material/Typography";
 
 const AddTask =({form ,setForm , mode, setMode,setOpen})=>{
     return(
-        <>
-
-        </>
+        <Grid component="form" noValidate onSubmit={handleSubmit}
+        sx={{ width:{xs:'250px',md:'450px'},border:'1px solid grey' , borderRadius:'2px', padding:'15px 25px'}}>
+            <Typography>Add Task</Typography>
+            <Grid sx={{display:'flex', flexDirection:'column',margin:'20px 0'}}>
+                <TextField 
+                    variant="standard" 
+                    label="Task" 
+                    sx={{width:{xs:'100%',md:'70%'}}}
+                    name="title"
+                    value={form.title} 
+                    onChange={handelChange}
+                    />
+                <TextField 
+                    variant="standard" 
+                    label="Description" 
+                    sx={{width:{xs:'100%',md:'70%'},marginTop:'10px'}}
+                    name="description" 
+                    value={form.description} 
+                    onChange={handelChange}
+                />
+            </Grid>
+            <Grid sx={{display:'flex',flexDirection:{xs:'row',md:'row-reverse'}, gap:'10px',marginTop:'40px'}}>
+                <Button 
+                onClick={handleCancel}
+                sx={{
+                    backgroundColor:'#bdbdbd',
+                    color:'White',
+                    fontSize:'12px',
+                    textTransform:"none",
+                    }}>
+                        Cancel
+                </Button>
+                <Button type="submit"
+                    disable={disable.toString()}
+                    sx={{
+                        backgroundColor:'#ef5350', 
+                        color:'White',fontSize:'12px',
+                        textTransform:"none" 
+                    }}
+                >
+                    {mode===true ? "Add": "Edit"}
+                </Button>
+            </Grid>
+        </Grid>
     )
 }
 
