70db9eb016f96f5c78859685323e27fe86949efa
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:20:18 +0430

    add logic

--- src/components/addTask/addTask.jsx
+++ src/components/addTask/addTask.jsx
@@ -7,6 +7,35 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 const AddTask =({form ,setForm , mode, setMode,setOpen})=>{
+
+    const [disable, setDisable] = useState(true)
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(mode && form.title.length > 0 && form.description.length > 0){
+            setDisable(!disable)
+            dispatch(AddItem(form))
+            setOpen(false)
+        }else if(!mode && form.title.length > 0 && form.description.length > 0){
+            setDisable(!disable)
+            dispatch(EditItem(form))
+            setOpen(false)
+        }
+        setForm({id:"" , title:"" , description:"" , status:""})
+        setMode(true)
+        setDisable(!disable)
+    }
+
+    const handelChange =(e)=>{
+        setForm({...form , [e.target.name] : e.target.value});
+    }
+
+    const handleCancel =()=>{
+        setOpen(false)
+    }
+
+
     return(
         <Grid component="form" noValidate onSubmit={handleSubmit}
         sx={{ width:{xs:'250px',md:'450px'},border:'1px solid grey' , borderRadius:'2px', padding:'15px 25px'}}>
