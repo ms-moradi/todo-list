2b18dd3e0dd3e3b0c58bd900a6271d0caf4a5f65
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:23:44 +0430

    add elements

--- src/components/massageDelete/massageDelete.jsx
+++ src/components/massageDelete/massageDelete.jsx
@@ -9,8 +9,24 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 const MassageDelete =()=>{
     return(
-        <>
-        </>
+        <Dialog open={openDialog} onClose={handleCloseDialog} >
+            <Grid container sx={{width:{xs:'250px', md:'370px'}, height:'150px', padding:'20px 10px 0 10px',gap:'20px',justifyContent:'space-between'}}>
+                <Grid container item sx={{display:'flex', flexRow:''}}>
+                    <ErrorOutlineIcon  sx={{color:'#ef5350', display:{xs:'none', md:'block'}}}/>
+                    <Typography sx={{fontSize:{xs:'14px', md:''}}}>
+                        Are you sure you want to delete this task?
+                    </Typography>
+                </Grid>
+                <Grid container sx={{justifyContent:'center', gap:'10px'}}>
+                    <Button onClick={()=>handelDelete(id)} sx={{backgroundColor:'#ef5350',color:'white',padding:'0 30px', height:'30px'}}>
+                        OK
+                    </Button>
+                    <Button onClick={handleCloseDialog} sx={{backgroundColor:'#bdbdbd', color:'white', padding:'0 10px', height:'30px'}}>
+                        Cancel
+                    </Button>
+                </Grid>
+            </Grid>  
+        </Dialog>
     )
 }
 
