12cb2c7a80f46aaeb012b07f6b8d13ad7fa9bab5
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:23:12 +0430

    create component

---
+++ src/components/massageDelete/massageDelete.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {useDispatch } from "react-redux";
+import {DeleteItem} from "../redux/todoReducer/todoReducer";
+import Grid from "@mui/material/Grid";
+import Dialog from '@mui/material/Dialog';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
+const MassageDelete =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default MassageDelete;
