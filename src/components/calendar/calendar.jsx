080fee917359b8c0af1ac0d1ce15946ba1903f49
ms.moradi <ms.moradi28@gmail.com>
2023-05-13 19:35:25 +0430

    create and imports

---
+++ src/components/calendar/calendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './calendar.css';
+
+const Calendar =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default Calendar;
